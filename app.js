7166e04f195f3e87b526a01b1b74669d5536b7a7
Pierre Paillard <paillard.pierre@gmail.com>
2021-03-17 13:29:46 +0100

    suppression des fichiers inutiles

--- app.js
+++
@@ -1,7 +0,0 @@
-import dayjs from 'dayjs';
-import { functionOne, functionTwo } from './myModule';
-
-window.console.log(dayjs().format('HH:mm:ss'));
-
-functionOne();
-functionTwo();
---
+++ index.js
@@ -0,0 +1,5 @@
+function test (){
+
+    
+    console.log("blabla");
+}
